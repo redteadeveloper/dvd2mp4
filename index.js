dc2dcf7cc9ff268b524bb5b18b38663e80b5a9d7
calvinjung <myadiuio@gmail.com>
2021-04-21 16:27:11 +0900

    Init

---
+++ index.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+require('electron-reload')(__dirname);
+
+let mainWindow
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 600,
+        height: 200,
+        frame: false,
+        transparent: true,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    mainWindow.loadFile('./src/index.html')
+    mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+    if (mainWindow === null) createWindow()
+})
---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+const { dialog } = require('electron').remote
+const hbjs = require('handbrake-js')
+
+function test() {
+
+    dialog.showOpenDialog({
+        properties: ['openDirectory']
+    }).then(function (response) {
+
+        if (!response.canceled) {
+            let dir = response.filePaths[0]
+
+            hbjs.spawn({ input: dir, output: 'output.mp4' })
+                .on('error', err => {
+                    // invalid user input, no video found etc
+                })
+                .on('progress', progress => {
+                console.log(progress.percentComplete + "% complete / ETA: " + progress.eta )
+            })
+
+        } else {
+            console.log("No folder selected")
+        }
+    })
+}
